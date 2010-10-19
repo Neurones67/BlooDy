c66bfe5d7a71e3bb8d3abadab4f7d5949c37f0f5
KaneRoot <kane.root@gmail.com>
2010-10-19 12:08:06 +0200

    Premier commit

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php 
+	echo('<?xml version=\'1.0\' encoding="UTF-8" ?>'); ?>
+	<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
+		"http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+
+	<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<title>Bienvenue sur le site !</title>
+	<meta http-equiv="Content-Type" content="text/html ; charset=UTF-8" />
+	<link rel="stylesheet" media="screen" type="text/css" href="LeStyle.css" />
+</head>
+<body>
+<div "head">
+</div>
+<div "menuConnexion">
+</div>
+<div "menuGauche">
+</div>
+<div "corps">
+</div>
+<div "menuDroite">
+</div>
+<div "foot">
+</div>
+</body>
+</html>
+
