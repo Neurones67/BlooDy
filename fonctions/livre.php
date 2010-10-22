63d1d7ac0657d255a646d979348921a2d8946b57
Marc <marc@moonscoop-fan.com>
2010-10-22 17:38:09 +0200

    Début de l'implémentation de la gestion des livres

---
+++ fonctions/livre.php
@@ -0,0 +1,28 @@
+<?php
+/*
+	Classe de gestion d'un livre (ou album)
+	Par Marc
+	Pour BlooDy
+	Licence GPL
+	22/10/10
+*/
+class Livre
+{
+	// Déclaration des attributs
+	private $lid;
+	private $nom:
+	private $nom;
+	private $isbn;
+	private $ean13;
+	private $date_publication;
+	private $lvalide;
+	private $description;
+	private $aid;
+	
+	public function __construct()
+	{
+	}
+	public function __destruct()
+	{
+	}
+}
