e352bf0ff73b628e27d530fc680021529a5f3564
Marc <marc@moonscoop-fan.com>
2010-10-22 15:45:02 +0200

    Ajout d'un fichier de test du site

---
+++ fonctions/test.php
@@ -0,0 +1,13 @@
+<?php
+/*
+	Script pour tester certaines choses sur le site
+	Have fun !
+	
+	Conseil : Gardez vos tests commentés, ne les supprimez pas, vous aurez surement besoin de les refaire :)
+*/
+// Test des mails : Résultat au 22/10/10 : Ça marche :)
+/*
+include('mail.php');
+$object=new EMail();
+$object->send('marc@moonscoop-fan.com','Test d\'envoi de mails','hééhooo ça marche ? :D');
+*/
