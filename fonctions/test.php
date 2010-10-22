6159fa814a9b3d0e1fad5982466a3d308c4e7f0f
Marc <marc@moonscoop-fan.com>
2010-10-22 17:28:50 +0200

    Test gestionnaire erreur MySQL concluant

--- fonctions/test.php
+++ fonctions/test.php
@@ -12,8 +12,16 @@ $object=new EMail();
 $object->send('marc@moonscoop-fan.com','Test d\'envoi de mails','hééhooo ça marche ? :D',"Marc");
 */
 
-// Test de la gestion d'erreur
+// Test de la gestion d'erreur : Résultat au 22/10/10 : Ça marche !
+/*
+bd@web:~/public_html/fonctions$ cat mysql_errors 
+[22/10/2010 - 17:26:54] Erreur MySQL : La table 'bd.trucs' n'existe pas
+Backtrace:
+#0  DB->query(SELECT * from trucs) called at [/home/bd/public_html/fonctions/test.php:19]
+*/
+/*
 include('db.php');
 include('password.php');
 $mysql=new DB($host,$username,$password,$base);
 $mysql->query('SELECT * from trucs');
+*/
