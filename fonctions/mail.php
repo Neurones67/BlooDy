cd2da8f3257a127a22ef29a2bfcdb1f713fe7ccf
Marc <marc@moonscoop-fan.com>
2010-10-22 15:23:04 +0200

    Première écriture de la classe d'envoi de mails

---
+++ fonctions/mail.php
@@ -0,0 +1,45 @@
+<?php
+/*
+	Classe permettant d'envoyer des emails
+	Par Marc
+	Pour BlooDy
+	Licence GPL
+	22/10/10
+	
+	Cette classe utilise la classe Mail de pear (http://pear.php.net/package/Mail)
+	Elle défini les variables par défaut utilisés par le serveur, ici, c'est SMTP sur le port 25 sur localhost (sans authentification)
+*/
+require_once('Mail.php'); 
+class EMail
+{
+	private $backend;
+	private $params;
+	private $mailobject;
+	private $headers;
+	public function __construct()
+	{
+		$this->backend="smtp";
+		$this->params=array(
+			"host"=>"localhost",
+			"port"=>"25");
+		$this->mailobject=Mail:factory($this->backend,$this->params);
+		$this->headers=array(
+			"From"=> "BlooDy <contact@bd.ceypasbien.com>",
+			"Sender"=> "BlooDy <contact@bd.ceypasbien.com>",
+			"Reply-To"=> "BlooDy <contact@bd.ceypasbien.com>",
+			"Content-Type" => "text/plain; charset=UTF-8",
+			"X-Mailer"=> "BlooDy",
+			"X-Origin"=> "http://bd.ceypasbien.com");
+	}
+	public function __destruct()
+	{
+	}
+	public function send($recipients,$subject,$message)
+	{
+		$lheaders=$this->headers;
+		$lheaders['To']=$recipients;
+		$lheaders['Subject']=$subject;	
+		return $this->mailobject->send($recipients,$lheaders,$message);
+	}
+	
+}
