2599a131401d4c8caddfccca2cb0bc5225b2ecc6
Marc <marc@moonscoop-fan.com>
2010-10-22 15:54:09 +0200

    Test concluant, on commente

--- fonctions/test.php
+++ fonctions/test.php
@@ -6,8 +6,8 @@
 	Conseil : Gardez vos tests commentés, ne les supprimez pas, vous aurez surement besoin de les refaire :)
 */
 // Test des mails : Résultat au 22/10/10 : Ça marche :)
-
+/*
 include('mail.php');
 $object=new EMail();
 $object->send('marc@moonscoop-fan.com','Test d\'envoi de mails','hééhooo ça marche ? :D',"Marc");
-
+*/
