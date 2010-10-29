c6b688f488f7fb71b5f84163e01840e43b9ffaea
Marc <marc@moonscoop-fan.com>
2010-10-30 01:21:06 +0200

    Remplissage du constructeur

--- fonctions/livre.php
+++ fonctions/livre.php
@@ -11,18 +11,54 @@ class Livre
 	// Déclaration des attributs
 	private $lid;
 	private $nom:
-	private $nom;
 	private $isbn;
 	private $ean13;
 	private $date_publication;
 	private $lvalide;
 	private $description;
-	private $aid;
+	private $auteur;
 	
-	public function __construct()
+	// Besoin d'une connexion MySQL
+	private $mysql;
+	
+	public function __construct($lid=0)
 	{
+		$this->mysql=requestObject('MySQL');
+		if($lid!=0)
+		{
+			$this->init_data($lid);
+		}
 	}
 	public function __destruct()
 	{
+		unset($this->nom);
+		unset($this->lid);
+		unset($this->ean13);
+		unset($this->date_publication);
+		unset($this->lvalide);
+		unset($this->description);
+		unset($this->aid);
+		unset($this->auteur);
+	}
+	private function init_data($lid)
+	{
+		// Initialise les attributs du livre avec le lid donné à partir de la base de donnée
+		$sql='SELECT l.nom,l.ean13,l.date_publication,l.lvalide,l.description,a.anom FROM livres l JOIN auteurs a ON l.aid=a.aid';
+		$req=$this->mysql->query($sql);
+		if($data=$req->fetch_object())
+		{
+			$this->lid=$lid;
+			$this->nom=$data->nom;
+			$this->isbn=$data->isbn;
+			$this->ean13=$data->ean13;
+			$this->date_publication=$data->date_publication;
+			$this->lvalide=$data->lvalide;
+			$this->auteur=$data->anom;
+			return true;
+		}
+		else
+		{
+			return false;
+		}
 	}
 }
