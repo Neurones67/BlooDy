0a306a818fcba53602572cb589f1411cd350da81
KaneRoot <kane.root@gmail.com>
2010-11-01 19:01:41 +0100

    Ajout de yaourt dans le corps du site

--- index.php
+++ index.php
@@ -30,6 +30,8 @@
 	</div>
 	<div id="corps">
 		<p>Voici le corps du site</p>
+		<p>Ajout de tout un tas de blabla totalement inutile. Bla.
+		La République du Yémen fut déclarée le 22 mai 19903. Ali Abdullah Saleh devint le président du Yémen et Ali Salim al-Beidh le premier ministre du Yémen. Une période transitoire de 30 mois fut fixée afin de fusionner les deux systèmes politiques et économiques. Un conseil présidentiel fut élu par les parlements respectifs des deux anciens pays. Un nouveau parlement, commun aux deux pays, le Parlement provisoire unifié, fut fondé. Il était composé de 159 membres originaires du Nord, de 111 membres originaires du Sud et de 31 membres indépendants nommés par le président du conseil.</p>
 	</div>
 	<div id="menuDroite">
 		<p>Voici le menu de suggestions</p>
