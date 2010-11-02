b4e55b65385c02c22a30e4aa9e9c5d94ee1fcf67
KaneRoot <kane.root@gmail.com>
2010-11-02 19:39:31 +0100

    Ajout des valeurs Identifiant et Mot de passe dans les champs de texte

--- index.php
+++ index.php
@@ -18,8 +18,8 @@
 	
 	<div id="menuConnexion">
 	<form method="POST" action="/connexion.php">
-		<input id="id_Connexion" type="text" name="id_Connexion" />
-		<input id="id_MotDePasse" type="password" name="id_MotDePasse" />
+		<input id="id_Identifiant" type="text" name="id_Connexion" value="Identifiant" />
+		<input id="id_MotDePasse" type="password" name="id_MotDePasse" value="Mot de passe" />
 		<input id="btn_Connexion" type="submit" value="Connexion" />
 		<a class="lien_MotDePassePerdu">Mot de passe perdu ?</a>
 		<input id="id_RechercheRapide" type="text" name="id_RechercheRapide" />
