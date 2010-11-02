bd14c6e1b9c58c04ee868ac69d1c7f2697bbf8e9
KaneRoot <kane.root@gmail.com>
2010-11-02 19:58:21 +0100

    Fin de séparation, foot et corps

--- index.php
+++ index.php
@@ -16,18 +16,10 @@
 	include_once('partial/entete.php');
 	include_once('partial/menuConnexion.php');
 	include_once('partial/menuGauche.php');
+	include_once('partial/menuDroite.php');
+	include_once('partial/corps.php');
+	include_once('partial/piedDePage.php');
 ?>	
-	<div id="menuDroite">
-		<p>Voici le menu de suggestions</p>
-	</div>
-	<div id="corps">
-		<p>Voici le corps du site</p>
-		<p>Ajout de tout un tas de blabla totalement inutile. Bla.<br />
-		La République du Yémen fut déclarée le 22 mai 1990. Ali Abdullah Saleh devint le président du Yémen et Ali Salim al-Beidh le premier ministre du Yémen. Une période transitoire de 30 mois fut fixée afin de fusionner les deux systèmes politiques et économiques. Un conseil présidentiel fut élu par les parlements respectifs des deux anciens pays. Un nouveau parlement, commun aux deux pays, le Parlement provisoire unifié, fut fondé. Il était composé de 159 membres originaires du Nord, de 111 membres originaires du Sud et de 31 membres indépendants nommés par le président du conseil.</p>
-	</div>
-	<div id="foot">
-		<p>Rien de spécial</p>
-	</div>
 </div>
 </body>
 </html>
