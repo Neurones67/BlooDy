81f53180f0e38f7bf70adfbaaf3ab930686645f7
Stéphane <psychomao@live.fr>
2010-11-05 13:18:41 +0100

    modif

--- partial/menuGauche.php
+++ partial/menuGauche.php
@@ -1,7 +1,7 @@
 	<div id="menuGauche">
 
 		<div id="menuGaucheinscr">
-		<a href="www.google.fr"><img src="/images/transparent" alt="transparentinscrire.png" /></a>
+		<a href="www.google.fr"><img src="/images/transparentinscrire.png" alt="transparentinscrire" /></a>
 		</div>
 		<p>Voici le menu de gauche</p>
 	</div>
