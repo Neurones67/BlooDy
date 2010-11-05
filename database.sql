9a22088522b65f42e09e7000c1797bea8056bc90
Marc <marc@moonscoop-fan.com>
2010-11-05 15:13:17 +0100

    Ajout de la table Appartient pour pouvoir connaitre les BDs d'un utilisateur

--- database.sql
+++ database.sql
@@ -1,58 +1,60 @@
--- MySQL dump 10.13  Distrib 5.1.49, for debian-linux-gnu (i486)
+-- phpMyAdmin SQL Dump
+-- version 3.3.7deb1
+-- http://www.phpmyadmin.net
 --
--- Host: localhost    Database: bd
--- ------------------------------------------------------
--- Server version	5.1.49-1
+-- Serveur: localhost
+-- Généré le : Ven 05 Novembre 2010 à 15:12
+-- Version du serveur: 5.1.49
+-- Version de PHP: 5.3.2-2
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
 --
--- Current Database: `bd`
+-- Base de données: `bd`
 --
 
-CREATE DATABASE /*!32312 IF NOT EXISTS*/ `bd` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_bin */;
-
-USE `bd`;
+-- --------------------------------------------------------
 
 --
--- Table structure for table `amis`
+-- Structure de la table `amis`
 --
 
-DROP TABLE IF EXISTS `amis`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `amis` (
+CREATE TABLE IF NOT EXISTS `amis` (
   `euid` int(11) DEFAULT NULL,
   `duid` int(11) DEFAULT NULL
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `amis`
+-- Contenu de la table `amis`
 --
 
-LOCK TABLES `amis` WRITE;
-/*!40000 ALTER TABLE `amis` DISABLE KEYS */;
-/*!40000 ALTER TABLE `amis` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `auteurs`
+-- Structure de la table `appartient`
 --
 
-DROP TABLE IF EXISTS `auteurs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `auteurs` (
+CREATE TABLE IF NOT EXISTS `appartient` (
+  `uid` int(11) DEFAULT NULL,
+  `lid` int(11) DEFAULT NULL,
+  `date_achat` int(11) DEFAULT NULL,
+  `etat` int(1) DEFAULT NULL,
+  `emplacement` text COLLATE utf8_bin
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
+
+--
+-- Contenu de la table `appartient`
+--
+
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `auteurs`
+--
+
+CREATE TABLE IF NOT EXISTS `auteurs` (
   `aid` int(11) NOT NULL AUTO_INCREMENT,
   `anom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `aphoto` varchar(50) COLLATE utf8_bin DEFAULT NULL,
@@ -60,121 +62,91 @@ CREATE TABLE `auteurs` (
   `adnaissance` date DEFAULT NULL,
   `avalide` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`aid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `auteurs`
+-- Contenu de la table `auteurs`
 --
 
-LOCK TABLES `auteurs` WRITE;
-/*!40000 ALTER TABLE `auteurs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `auteurs` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `demande_ami`
+-- Structure de la table `demande_ami`
 --
 
-DROP TABLE IF EXISTS `demande_ami`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `demande_ami` (
+CREATE TABLE IF NOT EXISTS `demande_ami` (
   `euid` int(11) DEFAULT NULL,
   `duid` int(11) DEFAULT NULL,
   `dadate` int(11) NOT NULL,
   `damessage` text COLLATE utf8_bin
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `demande_ami`
+-- Contenu de la table `demande_ami`
 --
 
-LOCK TABLES `demande_ami` WRITE;
-/*!40000 ALTER TABLE `demande_ami` DISABLE KEYS */;
-/*!40000 ALTER TABLE `demande_ami` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `editeurs`
+-- Structure de la table `editeurs`
 --
 
-DROP TABLE IF EXISTS `editeurs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `editeurs` (
+CREATE TABLE IF NOT EXISTS `editeurs` (
   `eid` int(11) NOT NULL AUTO_INCREMENT,
   `enom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`eid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `editeurs`
+-- Contenu de la table `editeurs`
 --
 
-LOCK TABLES `editeurs` WRITE;
-/*!40000 ALTER TABLE `editeurs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `editeurs` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `genre`
+-- Structure de la table `genre`
 --
 
-DROP TABLE IF EXISTS `genre`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `genre` (
+CREATE TABLE IF NOT EXISTS `genre` (
   `gid` int(11) NOT NULL AUTO_INCREMENT,
   `gnom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`gid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `genre`
+-- Contenu de la table `genre`
 --
 
-LOCK TABLES `genre` WRITE;
-/*!40000 ALTER TABLE `genre` DISABLE KEYS */;
-/*!40000 ALTER TABLE `genre` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `jetons_auth`
+-- Structure de la table `jetons_auth`
 --
 
-DROP TABLE IF EXISTS `jetons_auth`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `jetons_auth` (
+CREATE TABLE IF NOT EXISTS `jetons_auth` (
   `jid` int(11) NOT NULL AUTO_INCREMENT,
   `jdate` int(11) NOT NULL,
   `jip` varchar(11) COLLATE utf8_bin NOT NULL,
   `uid` int(11) DEFAULT NULL,
   PRIMARY KEY (`jid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `jetons_auth`
+-- Contenu de la table `jetons_auth`
 --
 
-LOCK TABLES `jetons_auth` WRITE;
-/*!40000 ALTER TABLE `jetons_auth` DISABLE KEYS */;
-/*!40000 ALTER TABLE `jetons_auth` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `livres`
+-- Structure de la table `livres`
 --
 
-DROP TABLE IF EXISTS `livres`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `livres` (
+CREATE TABLE IF NOT EXISTS `livres` (
   `lid` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `isbn` varchar(50) COLLATE utf8_bin DEFAULT NULL,
@@ -190,50 +162,38 @@ CREATE TABLE `livres` (
   `ajuid` int(11) DEFAULT NULL,
   `ajdate` int(11) DEFAULT NULL,
   PRIMARY KEY (`lid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `livres`
+-- Contenu de la table `livres`
 --
 
-LOCK TABLES `livres` WRITE;
-/*!40000 ALTER TABLE `livres` DISABLE KEYS */;
-/*!40000 ALTER TABLE `livres` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `series`
+-- Structure de la table `series`
 --
 
-DROP TABLE IF EXISTS `series`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `series` (
+CREATE TABLE IF NOT EXISTS `series` (
   `sid` int(11) NOT NULL AUTO_INCREMENT,
   `snom` varchar(50) COLLATE utf8_bin DEFAULT NULL,
   `svalide` tinyint(1) DEFAULT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `series`
+-- Contenu de la table `series`
 --
 
-LOCK TABLES `series` WRITE;
-/*!40000 ALTER TABLE `series` DISABLE KEYS */;
-/*!40000 ALTER TABLE `series` ENABLE KEYS */;
-UNLOCK TABLES;
+
+-- --------------------------------------------------------
 
 --
--- Table structure for table `utilisateurs`
+-- Structure de la table `utilisateurs`
 --
 
-DROP TABLE IF EXISTS `utilisateurs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `utilisateurs` (
+CREATE TABLE IF NOT EXISTS `utilisateurs` (
   `uid` int(11) NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(50) COLLATE utf8_bin NOT NULL,
   `motdepasse` varchar(50) COLLATE utf8_bin NOT NULL,
@@ -246,26 +206,10 @@ CREATE TABLE `utilisateurs` (
   `uetat` int(1) NOT NULL,
   `cvalidation` varchar(30) COLLATE utf8_bin DEFAULT NULL,
   PRIMARY KEY (`uid`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
-/*!40101 SET character_set_client = @saved_cs_client */;
+) ENGINE=MyISAM DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=1 ;
 
 --
--- Dumping data for table `utilisateurs`
+-- Contenu de la table `utilisateurs`
 --
 
-LOCK TABLES `utilisateurs` WRITE;
-/*!40000 ALTER TABLE `utilisateurs` DISABLE KEYS */;
-/*!40000 ALTER TABLE `utilisateurs` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2010-10-19 15:23:37
 
