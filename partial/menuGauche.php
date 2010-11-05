be19be408c94b37a00bdc4552c65e62485bdbdb4
Stéphane <psychomao@live.fr>
2010-11-05 13:14:47 +0100

    ajout d'un lien vers google provisoire

--- partial/menuGauche.php
+++ partial/menuGauche.php
@@ -1,5 +1,7 @@
 	<div id="menuGauche">
+
 		<div id="menuGaucheinscr">
+		<a href="www.google.fr"><img src="/images/transparent" alt="transparentinscrire.png" /></a>
 		</div>
 		<p>Voici le menu de gauche</p>
 	</div>
