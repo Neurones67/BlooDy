37e8fc816f076bd5a4589ac1580329a3dcf01987
KaneRoot <kane.root@gmail.com>
2010-11-12 15:53:00 +0100

    Modification des menuGauche, création d'ul pour chaque catégorie

--- fonctions/creer_formulaire.php
+++ fonctions/creer_formulaire.php
@@ -14,16 +14,25 @@ function creer_label_champ($label,$nom,$tailleChamp=null)
 	echo $ligne;
 }
 
+//fonction pour déterminer si une variable est renseignée et non vide
+function isDefined($val)
+{
+	$res = false;
+	if(isset($val) && $val != "")
+		$res = true;
+	return $res;
+}
+
 /* On souhaite une selection */
 /* tValeur est un tableau associatif value => nom affiché */
 function creer_selection($tValeur,$multiple,$nomSelection,$taille)
 {
 	$ligne="<select ";
-	if(isset($taille))
+	if(isDefined($taille))
 		$ligne.="size=\"".$taille."\" ";
-	if(isset($multiple))
+	if(isDefined($multiple))
 		$ligne.="multiple=\"".$multiple."\" ";
-	if(isset($nomSelection))
+	if(isDefined($nomSelection))
 		$ligne.="name=\"".$nomSelection."\" ";
 	$ligne.=">\n";
 
@@ -35,5 +44,14 @@ function creer_selection($tValeur,$multiple,$nomSelection,$taille)
 	
 	echo $ligne;
 }
-
+function creer_formulaire_inscription()
+{
+	echo "<form id=\"formulaire_inscription\" method=\"post\" action='' >";
+	creer_label_champ("pseudo","Pseudonyme");
+	creer_label_champ("motDePasse","Mod de passe");
+	creer_label_champ("confirmationMotDePasse","Confirmation du mot de passe");
+	creer_label_champ("email","E-mail");
+	creer_label_champ("confirmationEmail","Confirmation de l'adresse e-mail");
+	echo "</form>";
+}
 ?>
