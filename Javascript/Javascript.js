69b88cec0d57c532d8c6414b69d46dadf736f96f
Stéphane <psychomao@live.fr>
2010-11-21 14:51:30 +0100

    ajout de la fonction javascript de test si les deux éléments sont password sont identique

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -2,3 +2,23 @@ function Erase(nam)
 {
 		document.id_form.id_Connexion.value = "";
 }
+function VerificationMotDePasse
+{
+	var paragraphe = document.getElementById("formulaire_inscription.confirmationMotDePasse.erreurMotDePasse");
+	if(document.formulaire_inscription.motDePasse != document.formulaire_inscription.confirmationMotDePasse)
+	{
+                var old_contenu = paragraphe.firstChild;
+                paragraphe.removeChild(old_contenu);
+                //Ajout de l'information incorrect
+                var info = document.createTextNode("Erreur pas le même mot de passe");
+                paragraphe.appendChild(info);
+	}
+	else
+	{
+		var old_contenu = paragraphe.firstChild;
+                paragraphe.removeChild(old_contenu);
+                //Ajout de l'information incorrect
+                var info = document.createTextNode("Mot de passe correct");
+                paragraphe.appendChild(info);
+	} 
+}
