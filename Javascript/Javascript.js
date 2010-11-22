09a0cdef146227109c9e477f5939be506aa140fd
Marc <marc@moonscoop-fan.com>
2010-11-22 14:33:44 +0100

    Correction du javascript pour la vérification du mot de passe

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -1,24 +1,20 @@
-function Erase(nam)
+function erase(nam)
 {
 		document.id_form.id_Connexion.value = "";
 }
-function VerificationMotDePasse()
+function verificationMotDePasse()
 {
-	
-	if(document.formulaire_inscription.motDePasse != document.formulaire_inscription.confirmationMotDePasse)
+	var paragraphe = document.getElementById("erreurMotDePasse");
+	var old_contenu = paragraphe.firstChild;
+        paragraphe.removeChild(old_contenu);
+	if(document.getElementById('password').value != document.getElementById('password2').value)
 	{
-		var paragraphe = document.getElementById("erreurMotDePasse");
-                var old_contenu = paragraphe.firstChild;
-                paragraphe.removeChild(old_contenu);
                 //Ajout de l'information incorrect
-                var info = document.createTextNode("Erreur pas le même mot de passe");
+                var info = document.createTextNode("Erreur: Mots de passe différents");
                 paragraphe.appendChild(info);
 	}
 	else
 	{
-		var paragraphe = document.getElementById("erreurMotDePasse");
-		var old_contenu = paragraphe.firstChild;
-                paragraphe.removeChild(old_contenu);
                 //Ajout de l'information correct
                 var info = document.createTextNode("Mot de passe correct");
                 paragraphe.appendChild(info);
