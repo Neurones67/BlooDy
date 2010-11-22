adabc677bd87a7af57eea2ef13b28acbe7206b56
Marc <marc@moonscoop-fan.com>
2010-11-22 14:43:27 +0100

    Modification du style des messages d'erreur

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -4,7 +4,6 @@ function erase(nam)
 }
 function verificationMotDePasse()
 {
-	alert('J\'aime les frites');
 	var paragraphe = document.getElementById("erreurMotDePasse");
 	var old_contenu = paragraphe.firstChild;
         paragraphe.removeChild(old_contenu);
@@ -12,12 +11,16 @@ function verificationMotDePasse()
 	{
                 //Ajout de l'information incorrect
                 var info = document.createTextNode("Erreur: Mots de passe différents");
+                paragraphe.style.color="red";
+                paragraphe.style.fontWeight="bold";
                 paragraphe.appendChild(info);
 	}
 	else
 	{
                 //Ajout de l'information correct
                 var info = document.createTextNode("Mot de passe correct");
+                paragraphe.style.color="green";
+                paragraphe.style.fontWeight="bold";
                 paragraphe.appendChild(info);
 	} 
 }
