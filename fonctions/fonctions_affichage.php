a63e8d0b0a403ccecce4b404ea1f69aa0b3522e6
KaneRoot <kane.root@gmail.com>
2010-12-01 00:40:31 +0100

    Ajout d'includes

--- fonctions/fonctions_affichage.php
+++ fonctions/fonctions_affichage.php
@@ -1,8 +1,8 @@
 <?php
 
+include_once('livre.php');
 class Affichage
 {
-	include('livre.php');
 	// On passe à cette fonction le tableau contenant les informations utiles à afficher de façon très compacte
 	// On passe son identifiant unique (t[0]), son nom, son prénom, sa date de naissance
 	public static function affichage_tcompact_auteurs()
