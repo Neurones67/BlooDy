cd4dda6baefbd02c6a0197a3e598b59eff0ded63
Marc <marc@moonscoop-fan.com>
2010-12-03 17:00:14 +0100

    Ajout du genre dans la liste des bd

--- fonctions/fonctions_affichage.php
+++ fonctions/fonctions_affichage.php
@@ -77,6 +77,7 @@ class Affichage
 		foreach($tBDs as $tBD)
 		{
 			$res .= "<tr>"; 
+			$res .= "<td>".$tBD['gnom']."</td>";
 			$res .= "<td><a href='/affichage_complet_bds.html?id=" . $tBD['lid'] . "' >".$tBD['nom']."</a></td>\n";
 			$res .= "<td>" . $tBD['anom'] . "</td><td><a href='/affichage_complet_auteurs.html?id=" . $tBD['aid'] . "' />" . $tBD['aprenom'] . "</a></td><td>" . $tBD['enom']."</td><td>" . $tBD['isbn'] . "</td>\n";
 			$res .= "<td>".$tBD['ean13']."</td>\n";
