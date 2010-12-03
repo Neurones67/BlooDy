6c34322368898facf21b8686e1b370510a196604
Stéphane <psychomao@live.fr>
2010-12-03 17:53:51 +0100

    test sur la function visible et cache

--- Javascript/Javascript.js
+++ Javascript/Javascript.js
@@ -61,9 +61,9 @@ function verificationConformiteEmail()
 	}
 	paragraphe.appendChild(info);
 }
-function cacheGenre(Id)
+function cacheGenre()
 {
-	var p = document.getElementById(Id);
+	var p = document.getElementById(id_auteurs);
 	if(p.document.style.display="visible")
 	{
 		p.document.style.display="none";
@@ -71,7 +71,7 @@ function cacheGenre(Id)
 }
 function visibleGenre()
 {
-	var p = document.getElementById(Id);
+	var p = document.getElementById(id_auteurs);
 	if(p.document.style.display="none")
 	{
 		p.document.style.display="visible";
