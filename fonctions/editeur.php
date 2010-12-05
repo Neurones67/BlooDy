19e3603c2c06d5aafc8543260a4b75381f4225d1
Marc <marc@moonscoop-fan.com>
2010-12-05 21:49:50 +0100

    Ajout de la classe de gestion des editeurs

---
+++ fonctions/editeur.php
@@ -0,0 +1,86 @@
+<?php
+/*
+ *	Classe de gestion des Éditeurs
+ *	Par Marc
+ *	Pour BlooDy
+ *	5/12/10
+ */
+ class Editeur
+ {
+ 	private $eid;
+ 	private $enom;
+ 	private $evalide;
+ 	
+ 	private $mysql;
+ 	public function __construct($eid=0)
+ 	{
+ 		$this->mysql=requestObject('MySQL');
+ 		if($eid>0)
+ 		{
+ 			$this->init_data($uid);
+ 		}
+ 		else
+ 		{
+ 			$this->eid=-1;
+ 			$this->enom="";
+ 			$this->evalide=-1;
+ 		}
+ 	}
+ 	public function __destruct()
+ 	{
+ 		unset($this->eid);
+ 		unset($this->enom);
+ 	}
+ 	
+ 	private function init_data($eid)
+ 	{
+ 		$sql='SELECT enom,evalide FROM editeurs WHERE eid='.$eid;
+ 		$req=$this->mysql->query($sql);
+ 		if($data=$req->fetch_object())
+ 		{
+ 			$this->eid=$eid;
+ 			$this->enom=$data->enom;
+ 			$this->evalide=$data->evalide;
+ 		}
+ 	}
+ 	public function getEid()
+ 	{
+ 		return $this->eid;
+ 	}
+ 	public function getNom()
+ 	{
+ 		return $this->enom;
+ 	}
+ 	public function getValide()
+ 	{
+ 		return $this->evalide;
+ 	}
+ 	public function estValide()
+ 	{
+ 		return $this->evalide>0;
+ 	}
+ 	
+ 	public function add($enom)
+ 	{
+ 		$enom=$this->mysql->real_escape_string($enom);
+ 		$sql='INSERT INTO editeurs (enom) VALUES ("'.$enom.'")';
+ 		return $this->mysql->query($sql);
+ 	}
+ 	public function delete($eid)
+ 	{
+ 		$eid=intval($eid);
+ 		$sql='DELETE FROM editeurs WHERE eid='.$eid;
+ 		return $this->mysql->query($sql);
+ 	}
+ 	public function recherche($enom)
+ 	{
+ 		$enom=$this->mysql_real_escape_string($enom);
+ 		$sql="SELECT eid FROM editeurs WHERE UPPER(enom) LIKE UPPER('%".$enom."%')";
+ 		return queryToArray($this->mysql->query($sql));
+ 	}
+ 	public function liste()
+ 	{
+ 		$sql='SELECT eid,enom,evalide FROM editeurs';
+ 		return queryToArray($this->mysql->query($sql));
+ 	}
+ }
--- fonctions/fonctions.php
+++ fonctions/fonctions.php
@@ -32,6 +32,7 @@ require_once('db.php');
 require_once('utilisateurs.php'); 
 require_once('livre.php'); 
 require_once('auteur.php');
+require_once('editeur.php');
 require_once('fonctions_affichage.php');
 require_once('mail.php');
 
