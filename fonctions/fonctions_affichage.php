8c21c5dbd7104f571fd34b8ad64875ddd568ccf5
KaneRoot <kane.root@gmail.com>
2010-12-05 22:35:13 +0100

    Ajout d'une fonction non utilisée

--- fonctions/fonctions_affichage.php
+++ fonctions/fonctions_affichage.php
@@ -131,5 +131,45 @@ class Affichage
 
 		return $res;
 	}
+	public function resultat_recherche()
+	{
+		$livre =requestObject('Livre');
+		$tBDs = $livre->listBD();
+		$user=requestObject('Utilisateurs');
+		// Les titres de chaque colonne
+
+		$res = "";
+		$res .= "<table id='bds'><tr><th>Genre</th><th>Nom de la Bande Dessinée</th><th>Nom de l'auteur</th><th>Prénom de l'auteur</th>\n";
+		$res .= "<th>Nom de l'éditeur</th><th>ISBN</th><th>EAN13</th>";
+		if($user->estConnecte())
+			$res .= "<th>Ajouter à ma collection</th>";
+		$res .= "</tr>\n";
+
+		foreach($tBDs as $tBD)
+		{
+			$res .= "<tr>"; 
+			$res .= "<td><img src='/images/genre_".$tBD['gnom'].".png' alt='image_genre' /></td>";
+			$res .= "<td><a href='/livre-".$tBD['lid'].".html' >".$tBD['nom']."</a></td>\n";
+			$res .= "<td><a href='/auteur-" . $tBD['aid'] . ".html' />" . $tBD['anom'] . "</a></td><td>". $tBD['aprenom'] . "</a></td><td>" . $tBD['enom']."</td><td>" . $tBD['isbn'] . "</td>\n";
+			$res .= "<td>".$tBD['ean13']."</td>\n";
+			
+			if($user->estConnecte())
+			{
+				if($tBD['etat']<1) // Si on a pas la BD
+				{
+					$res .= "<td><input type='checkbox' name='BD[]' value='" . $tBD['lid'] . "' /></td>";
+				}
+				else
+				{
+					$res .= "<td>;)</td>";
+				}
+			}
+
+			$res .= "</tr>\n";
+		}
+		$res .= "</table>\n";
+
+		return $res;
+	}
 }
 ?>
