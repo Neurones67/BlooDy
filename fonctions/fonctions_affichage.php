dd3c3444b3fa7fbf4b438f1a17f7de27f303389b
KaneRoot <kane.root@gmail.com>
2011-01-08 19:05:34 +0100

    Utilisateur fait ou non partie des amis

--- fonctions/fonctions_affichage.php
+++ fonctions/fonctions_affichage.php
@@ -261,12 +261,10 @@ class Affichage
 
 			if($user->estConnecte())
 			{
-				/* À modifier quand je saurai quoi mettre
-				if($tami['etat']<1) // Si la personne ne fait pas encore partie des amis
+				if(!empty($tami['date_ajout'])) // Si la personne ne fait pas encore partie des amis
 					$res .= "<td><input type='checkbox' name='amis[]' value='" . $tami['uid'] . "' /></td>";
 				else
 					$res .= "<td>Vous possédez déjà cette personne</td>";
-				*/
 			}
 				$res .= "</tr>\n";
 		}
