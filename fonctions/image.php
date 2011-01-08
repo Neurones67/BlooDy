87d349a5a66886316bde51317c6019c71a66ae16
Marc <marc@moonscoop-fan.com>
2011-01-08 21:06:15 +0100

    petit artefact...

--- fonctions/image.php
+++ fonctions/image.php
@@ -41,7 +41,6 @@ class Image
 			$pathredim=$folder.$filename.basename($file['name'],'.'.$info['extension']).'_'.$xlimit.'_'.$ylimit.'.'.$info['extension'];
 			move_uploaded_file($file['tmp_name'],$npath);
 			$this->redim_img($npath,$xlimit,$ylimit,$pathredim);
-			$pathnav=
 			return str_replace(ROOT,'/',$npath);
 		}
 		else
