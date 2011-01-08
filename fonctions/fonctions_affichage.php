ea5d950d50cf5d2a776165ad4a682500de6d7184
Marc <marc@moonscoop-fan.com>
2011-01-08 23:08:02 +0100

    Plus de fatal error

--- fonctions/fonctions_affichage.php
+++ fonctions/fonctions_affichage.php
@@ -108,7 +108,7 @@ class Affichage
 	public function creerLiensLettres($nomPage)
 	{
 		$res = "<p>";
-		$tCar = {"0","1","2","3","4","5","6","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"};
+		$tCar = array("0","1","2","3","4","5","6","6","7","8","9","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z");
 		foreach($tCar as $lettre)
 		{
 			if(isset($_GET['lettre']) && $_GET['lettre'] != $lettre)
